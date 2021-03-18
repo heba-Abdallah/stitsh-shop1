ef4c05fcdeadaef4a8d56369c57e0ece531ea6bb
heba-Abdallah <heba.essam.m@hotmail.com>
2021-03-18 15:39:27 +0200

    make my javasrribt

---
+++ java/app.js
@@ -0,0 +1,12 @@
+ alert("hello");
+
+ var work= prompt("what do you want to see?");
+
+ if (work=="Thobe"){
+     document.write('<img src="photo/work 3.jpg" width="200px"/>');
+     
+ }else{
+     document.write('<img src="photo/work 1.jpg"  width="200px"/>'); 
+
+
+ }
