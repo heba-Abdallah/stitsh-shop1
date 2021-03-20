81d87df4181b33563c117edfeb72e90a71f3559a
heba-Abdallah <heba.essam.m@hotmail.com>
2021-03-20 21:18:43 +0200

    changing my page

--- java/app.js
+++ java/app.js
@@ -1,12 +1,13 @@
  alert("hello");
 
- var work= prompt("what do you want to see?");
+ var work= prompt("what do you want to see? Thobe or facemask?");
 
  if (work=="Thobe"){
      document.write('<img src="photo/work 3.jpg" width="200px"/>');
-     
- }else{
+
+ }else if(work=="facemask"){
      document.write('<img src="photo/work 1.jpg"  width="200px"/>'); 
 
-
+ }else{
+     document.write('<img src="photo/work 2.jpg" width="200px"/>')
  }
