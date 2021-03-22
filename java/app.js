b14b1dd2c7953b79a63b78db50f1ca825380068f
heba-Abdallah <heba.essam.m@hotmail.com>
2021-03-22 16:30:01 +0200

    adding css

--- java/app.js
+++ java/app.js
@@ -2,24 +2,29 @@ alert("hello");
 
 var work = prompt("what do you want to see? thobe or bookmark?");
 
-while (work !== 'thobe' && work !== 'bookmark') {
-    work = prompt('please choose Thobe/bookmark');
+var getwork = function () {
+    var stitsh = '';
+    var theResult = '';
+    while (work !== 'thobe' && work !== 'bookmark') {
+        work = prompt('please choose thobe/bookmark');
+    }
+    var numOfItems = prompt('How many items do you want?');
+
+    
+
+
+    if (work == 'thobe') {
+        stitsh = ('<img src="photo/work 3.jpg" width="200px"/>');
+
+    } else if (work == 'bookmark') {
+        stitsh = ('<img src="photo/work 2.jpg"  width="200px"/>');
+
+    }
+
+    for (var i = 0; i < numOfItems; i++) {
+        theResult += stitsh
+    }
+    console.log(theResult);
+    return theResult;
+
 }
-var numOfItems = prompt('How many items do you want?');
-
-var stitsh = '';
-var theResult = '';
-
-if (work == 'thobe') {
-    stitsh = ('<img src="photo/work 3.jpg" width="200px"/>');
-
-} else if (work == 'bookmark') {
-    stitsh = ('<img src="photo/work 2.jpg"  width="200px"/>');
-
-}
-
-for (var i = 0; i < numOfItems; i++) {
-    theResult += stitsh
-}
-console.log(theResult);
-document.write(theResult);
